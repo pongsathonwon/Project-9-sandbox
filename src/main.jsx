fb27cec65be9d0367fb3755c72414bd2b69e6675
pongsathonwon <men1234-2008@hotmail.com>
2024-10-13 18:16:55 +0700

    config routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./pages/Layout.jsx";
+import Home from "./pages/Home.jsx";
+import Clothing from "./pages/Clothing.jsx";
+import Cart from "./pages/Cart.jsx";
 
-createRoot(document.getElementById('root')).render(
+const ROUTES = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", children: <Home /> },
+      { path: "/clothing/:type", children: <Clothing /> },
+      { path: "/cart", children: <Cart /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={ROUTES} />
+  </StrictMode>
+);
