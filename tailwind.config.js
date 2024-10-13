8603f6b1408aaf527a9d85b3083e38e4433f5ce4
pongsathonwon <men1234-2008@hotmail.com>
2024-10-13 20:32:10 +0700

    add color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,30 @@ export default {
       "extra-bold": "800",
       black: "900",
     },
+    colors: {
+      primary: {
+        DEFAULT: "#DEF81C",
+        50: "#FBFFE7",
+        100: "#F6FCC3",
+        300: "#E5F96E",
+        400: "#DFF547",
+        500: "#DEF81C",
+        700: "#C1CD00",
+      },
+      secondary: {
+        DEFAULT: "#222222",
+        50: "#FAFAFA",
+        100: "#F5F5F5",
+        300: "#E1E1E1",
+        500: "#9F9F9F",
+        700: "#626262",
+        900: "#222222",
+      },
+      success: "#13CE66",
+      info: "#3366FF",
+      warn: "#FFB020",
+      danger: "#FF000D",
+    },
     screens: {
       md: "768px",
       // => @media (min-width: 768px) { ... }
