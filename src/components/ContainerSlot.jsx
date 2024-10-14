fca6b514827864ea83ca7d6d326babc86a24e83f
pongsathonwon <men1234-2008@hotmail.com>
2024-10-14 19:57:52 +0700

    add props to center label position

--- src/components/ContainerSlot.jsx
+++ src/components/ContainerSlot.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-function ContainerSlot({ containerLable, children }) {
+const POSITION = {
+  start: "",
+  center: "mx-auto",
+};
+
+function ContainerSlot({
+  containerLabel,
+  containerLabelPosition = "start",
+  children,
+}) {
   return (
     <div className="flex flex-col gap-16">
-      <h5>{containerLable}</h5>
+      <h5 className={POSITION[containerLabelPosition]}>{containerLabel}</h5>
       <div className="flex gap-10 overflow-x-auto">{children}</div>
     </div>
   );
