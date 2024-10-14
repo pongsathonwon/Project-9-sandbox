51afaf186f1ae444aa9d80ab7ec0a8a2bff7353d
pongsathonwon <men1234-2008@hotmail.com>
2024-10-14 19:47:26 +0700

    add sidebar draft1

---
+++ src/components/header/Sidebar.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { genClothingList, navlist } from "./navlist";
+import Arrow from "../Icon/Arrow";
+
+const ListItem = ({ children }) => {
+  return (
+    <li className="h-12 w-64 flex items-center justify-between">{children}</li>
+  );
+};
+
+const LayoutSidebar = ({ zIndex = "z-20", children }) => {
+  return (
+    <div className={`bg-white rounded-r-2xl pt5 ${zIndex}`}>{children}</div>
+  );
+};
+
+const SecondarySidebar = () => {};
+
+function Sidebar({ isShow, onClick }) {
+  const navigate = useNavigate();
+  const { type } = useParams();
+  console.log(type);
+  return (
+    <div
+      tabIndex={0}
+      className={`fixed top-0 left-0 right-0 h-screen bg-black bg-opacity-50 flex transition-all duration-300 ${
+        isShow ? "" : "-translate-x-full"
+      }`}
+      onClick={onClick}
+    >
+      <div className=" bg-white z-20 rounded-r-2xl pt-5">
+        <button
+          className={`font-bold capitalize flex gap-8 items-center w-full px-4 h-12 ${
+            type ? "border-b border-secondary-300 text-2xl" : "text-lg"
+          }`}
+          onClick={onClick}
+        >
+          {type ? (
+            <>
+              <Arrow direction="left" />
+              {type}
+            </>
+          ) : (
+            <span className="text-lg ml-4">home</span>
+          )}
+        </button>
+        <ul className="flex flex-col gap-2 text-lg font-semibold px-8 capitalize">
+          {navlist.map((n) => (
+            <ListItem key={n}>
+              <Link
+                className="flex items-center justify-between w-full"
+                to={genClothingList(n)}
+              >
+                {n} <Arrow direction="right" />
+              </Link>
+            </ListItem>
+          ))}
+        </ul>
+      </div>
+      <SecondarySidebar></SecondarySidebar>
+    </div>
+  );
+}
+
+export default Sidebar;
