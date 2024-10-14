6e8a204a2485c69a447a8de49ba6c33e13fce973
pongsathonwon <men1234-2008@hotmail.com>
2024-10-14 20:38:35 +0700

    add more navlist

--- src/components/header/navlist.js
+++ src/components/header/navlist.js
@@ -1,3 +1,20 @@
 export const navlist = ["men", "women", "kid", "shoes", "accessories"];
 
 export const genClothingList = (label) => `/clothing/${label}`;
+
+export const secondaryNavlist = [
+  "top",
+  "bottom",
+  "dress & jumpsuits",
+  "accessories",
+  "collections",
+];
+
+export const tertiaryNavlist = [
+  "all items",
+  "T-shirt",
+  "cardigion",
+  "kintwear & sweaters",
+  "sweatshirt & hoodies",
+  "fleece",
+];
