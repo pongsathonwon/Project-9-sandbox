342f7e1f1edfe3174a65fc7f8b991667a9617197
pongsathonwon <men1234-2008@hotmail.com>
2024-10-14 20:38:22 +0700

    extract layout sidebar

--- src/components/header/Sidebar.jsx
+++ src/components/header/Sidebar.jsx
@@ -11,11 +11,9 @@ const ListItem = ({ children }) => {
 
 const LayoutSidebar = ({ zIndex = "z-20", children }) => {
   return (
-    <div className={`bg-white rounded-r-2xl pt5 ${zIndex}`}>{children}</div>
+    <div className={`bg-white rounded-r-2xl pt-5 ${zIndex}`}>{children}</div>
   );
 };
-
-const SecondarySidebar = () => {};
 
 function Sidebar({ isShow, onClick }) {
   const navigate = useNavigate();
@@ -29,7 +27,8 @@ function Sidebar({ isShow, onClick }) {
       }`}
       onClick={onClick}
     >
-      <div className=" bg-white z-20 rounded-r-2xl pt-5">
+      {/* primary sidebar */}
+      <LayoutSidebar>
         <button
           className={`font-bold capitalize flex gap-8 items-center w-full px-4 h-12 ${
             type ? "border-b border-secondary-300 text-2xl" : "text-lg"
@@ -45,22 +44,43 @@ function Sidebar({ isShow, onClick }) {
             <span className="text-lg ml-4">home</span>
           )}
         </button>
-        <ul className="flex flex-col gap-2 text-lg font-semibold px-8 capitalize">
-          {navlist.map((n) => (
-            <ListItem key={n}>
-              <Link
-                className="flex items-center justify-between w-full"
-                to={genClothingList(n)}
-              >
-                {n} <Arrow direction="right" />
-              </Link>
-            </ListItem>
-          ))}
-        </ul>
-      </div>
-      <SecondarySidebar></SecondarySidebar>
+        <div className=" bg-white z-20 rounded-r-2xl pt-5">
+          <ul className="flex flex-col gap-2 text-lg font-semibold px-8 capitalize">
+            {navlist.map((n) => (
+              <ListItem key={n}>
+                <Link
+                  className="flex items-center justify-between w-full"
+                  to={genClothingList(n)}
+                >
+                  {n} <Arrow direction="right" />
+                </Link>
+              </ListItem>
+            ))}
+          </ul>
+        </div>
+      </LayoutSidebar>
     </div>
   );
 }
 
 export default Sidebar;
+
+{
+  /*
+          <button
+          className={`font-bold capitalize flex gap-8 items-center w-full px-4 h-12 ${
+            type ? "border-b border-secondary-300 text-2xl" : "text-lg"
+          }`}
+          onClick={onClick}
+        >
+          {type ? (
+            <>
+              <Arrow direction="left" />
+              {type}
+            </>
+          ) : (
+            <span className="text-lg ml-4">home</span>
+          )}
+        </button>
+*/
+}
