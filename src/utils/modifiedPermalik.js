68a3a17ac87cf801a665c0be797b4cb05cbba2c6
pongsathonwon <men1234-2008@hotmail.com>
2024-10-15 18:12:50 +0700

    add filter

--- src/utils/modifiedPermalik.js
+++ src/utils/modifiedPermalik.js
@@ -1,18 +1,39 @@
 /*
-    varients: {skuCode: string, color: string, size: sting, remains: number, colorCode: "#hex"}[]
-    field = skuCode | color | size | remains | colorCode
+    Varients: {skuCode: string, color: string, size: sting, remains: number, colorCode: "#hex"}[]
+    ModifiedVarients = 
+
+    flow: http response getbypermalink => {
+        id, name, 
+        skuCode, >> not valid for http call 
+        permalink, description, 
+        price: number, promotionalPrice: number, 
+        categories: string[], collection, rating: number, 
+        imageUrls: sting[], varients:Varients
+    } => tranform => {
+        id, name, description, 
+        price, promotionalPrice, rating 
+        categories, collection, imageUrls
+        modifiedVarient: {color: {color, colorCode}[], size: size[]}
+        endpointResult: {remains, skuCode} || (size, color) => {remains, skuCode}
+    }
 */
 
-const extractColor = ({ color, colorCode }) => ({ color, colorCode });
-const extractOthers = (field) => (varient) => varient[field];
-
 export const conbineExtractFn = (varients) => {
-  const extractPossibleColor = varients.map(extractColor);
-  const extractPossibleSize = varients.map(extractOthers("size"));
-  const extractPossibleRemains = varients.map(extractOthers("remains"));
+  const extractPossibleColor = varients.map(({ color, colorCode }) => ({
+    color,
+    colorCode,
+  }));
+  const extractPossibleSize = varients.map(({ size }) => size);
   return {
     color: extractPossibleColor,
     size: extractPossibleSize,
-    remains: extractPossibleRemains,
   };
+};
+
+export const filterProductAmount = (color, size) => (varients) => {
+  const [{ remains, skuCode }] = varients
+    .filter((varient) => varient.color === color && varient.size === size)
+    .map(({ remains, skuCode }) => ({ remains, skuCode }));
+  if (!remains || !skuCode) throw new Error("no requested product");
+  return { remains, skuCode };
 };
