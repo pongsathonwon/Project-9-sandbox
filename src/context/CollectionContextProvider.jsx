bca49fc0b270c1dcf352cf82d717c44ec9c344ec
pongsathonwon <men1234-2008@hotmail.com>
2024-10-17 17:40:27 +0700

    add possible list for render collection

--- src/context/CollectionContextProvider.jsx
+++ src/context/CollectionContextProvider.jsx
@@ -16,6 +16,13 @@ export const useCollectionContext = () => {
 function CollectionContextProvider({ children }) {
   const { data, erorr, isLoading, setError, setLoading, setSuccess } =
     useBaseState();
+
+  const possibleList =
+    data.map(({ name, permalink }) => ({
+      label: name,
+      path: permalink,
+    })) ?? [];
+
   React.useEffect(() => {
     setLoading();
     (async () => {
@@ -29,7 +36,7 @@ function CollectionContextProvider({ children }) {
   }, []);
   return (
     <CollectionContext.Provider
-      value={{ collectionList: data, erorr, isLoading }}
+      value={{ collectionList: data, possibleList, erorr, isLoading }}
     >
       {children}
     </CollectionContext.Provider>
