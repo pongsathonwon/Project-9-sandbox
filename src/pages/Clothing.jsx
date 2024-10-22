53ab365c40e7854160eb9dc91c87a0006dc218fd
pongsathonwon <men1234-2008@hotmail.com>
2024-10-22 22:24:41 +0700

    Revert "try api call"
    This reverts commit 78eb46ca56243f57a9c5f0d4f0cf1a5784d1b083.

--- src/pages/Clothing.jsx
+++ src/pages/Clothing.jsx
@@ -1,13 +1,6 @@
 import React from "react";
-import { useClothingContext } from "../context/ClothingContextProvider";
-import { useParams } from "react-router-dom";
 
 function Clothing() {
-  const { data, setQuery } = useClothingContext();
-  const { type } = useParams();
-  const [cat, col] = type.split("&");
-  setQuery(cat ?? null, col ?? null);
-  console.table(data);
   return <div>Clothing</div>;
 }
 
