3f20c214195fdd2950bbbeb7d97c7763aa2b6905
pongsathonwon <men1234-2008@hotmail.com>
2024-10-27 00:20:28 +0700

    extract collection card, add loading skeleton, add fetching + switch collection

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -5,37 +5,30 @@ import FeaturedProducts from "./FeaturedProducts"; // Import the FeaturedProduct
 import ContainerSlot from "./ContainerSlot";
 
 const Mainpage = () => {
-  const { collectionList, isLoading } = useCollectionContext();
-
-  if (isLoading || !collectionList) return <div>loading....</div>;
-  const [page, setPage] = useState(0);
-  const { name, description, items, permalink } = collectionList[1];
-  useEffect(() => {
-    const id = setInterval(() => {
-      if (page > 1) {
-        setPage(0);
-      }
-      setPage((p) => p + 1);
-    }, 1 * 1000);
-    return () => clearInterval(id);
-  }, []);
   return (
-    <div className="w-full h-auto relative bg-white">
-      {/* Header Image Section */}
-      <div className="w-full h-[420px] lg:h-[600px] relative">
-        <img
-          className="w-full h-full object-cover"
-          src={headerPicture}
-          alt="Header Image"
-        />
+    <>
+      <div className="w-full h-auto relative bg-white">
+        {/* Header Image Section */}
+        <div className="w-full h-[420px] lg:h-[600px] relative">
+          <img
+            className="w-full h-full object-cover"
+            src={headerPicture}
+            alt="Header Image"
+          />
+        </div>
+
+        {/* Collection Section */}
+        <CollectionSection />
+
+        {/* Featured Products Section */}
+        <section className="py-16 px-[1.125rem] lg:px-32 items-center w-screen">
+          <ContainerSlot
+            containerLabel="Feature Products"
+            containerLabelPosition="center"
+          />
+        </section>
       </div>
-
-      {/* Collection Section */}
-      <CollectionSection />
-
-      {/* Featured Products Section */}
-      <ContainerSlot />
-    </div>
+    </>
   );
 };
 
