0cf36bb06c3db20e4cc5448811bd303fdaba3296
pongsathonwon <men1234-2008@hotmail.com>
2024-10-29 12:02:21 +0700

    extract card skeleton(SkCard)

--- src/components/ContainerSlot.jsx
+++ src/components/ContainerSlot.jsx
@@ -5,39 +5,11 @@ import { getData } from "../utils/apiHandler";
 import Skeleton from "./Skeleton";
 import StarRating from "./StarRating";
 import { loadLocal, LOCALSTORAGE_KEY, saveToLocal } from "../utils/loacl";
+import SkCard from "./SkCard";
 
 const POSITION = {
   start: "",
   center: "mx-auto",
-};
-
-const SkCard = () => {
-  return (
-    <div
-      style={{ width: "267px", height: "453px" }}
-      className="flex flex-col justify-between"
-    >
-      <Skeleton bgColor="dark" className="w-[267px] aspect-square" />
-      <Skeleton
-        bgColor="light"
-        style={{ height: "3rem", width: `${40 * (1 + Math.random())}%` }}
-        className="rounded-lg"
-      />
-      <Skeleton
-        style={{ height: "1.25rem", width: `${50 * (1 + Math.random())}%` }}
-        className="rounded-lg"
-      />
-      <div className="animate-pulse">
-        <StarRating rating={0} />
-      </div>
-      <div>
-        <Skeleton
-          style={{ width: `${20 * (1 + Math.random())}%` }}
-          className="ml-auto h-12 rounded-lg"
-        />
-      </div>
-    </div>
-  );
 };
 
 function ContainerSlot({
