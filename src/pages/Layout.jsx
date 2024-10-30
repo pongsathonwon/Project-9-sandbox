b73eecab4d90d41f18dd86e7b058fd89accc50b1
pongsathonwon <men1234-2008@hotmail.com>
2024-10-30 11:11:57 +0700

    scroll to top on page change

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Header from "../components/header/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 
 function Layout() {
+  const location = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [location.pathname]);
   return (
     <>
       <Header />
