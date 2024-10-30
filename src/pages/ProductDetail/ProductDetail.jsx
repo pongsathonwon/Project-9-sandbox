6f3fba54891920389a71ef560d7b4a06f7b605da
Punnawat <punnawat.mungkalarungsi@gmail.com>
2024-10-30 09:49:01 +0700

    edit permalink handle

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import LoadingScreen from "./LoadingScreen";
 import ProductModal from "./ProductModal";
-import { getUniqueValue, permalinks, getWeightSize } from "../../utils/productDetail"
+import { getUniqueValue, getWeightSize } from "../../utils/productDetail"
 import { useParams } from "react-router-dom";
 import ContainerSlot from "../../components/ContainerSlot";
 import NextButt from "./NextButt";
@@ -9,7 +9,7 @@ import SelectionComponent from "./SelectionComponent";
 import DescriptionComponent from "./DescriptionComponent";
 
 function ProductDetail() {
-  const { permalink } = useParams() || permalinks[0];
+  const { permalink } = useParams();
   const [finishLoading, setFinishLoading] = React.useState(false);
   // must have the productdetail object
   const [productdetail, setProductDetail] = React.useState({});
