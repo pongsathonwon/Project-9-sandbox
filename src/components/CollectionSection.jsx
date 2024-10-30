637e5dfdf9af89bb272a12a7301e2e8d2cb1780f
pongsathonwon <men1234-2008@hotmail.com>
2024-10-31 00:32:08 +0700

    change collection size

--- src/components/CollectionSection.jsx
+++ src/components/CollectionSection.jsx
@@ -7,24 +7,24 @@ import Skeleton from "./Skeleton";
 
 const ColloctionCard = ({ title, imageUrl, description, permalink }) => {
   return (
-    <div className="relative w-[345px] h-[300px] md:w-[575px] md:h-[500px] p-4 bg-gradient-to-b from-[rgba(0,0,0,)] to-black flex-col justify-end items-center gap-4 inline-flex">
+    <div className="relative min-w-[343px] h-[500px] max-w-[575px] md:h-[500px] md:w-[575px] xl:h-[462px] 2xl:h-[500px] xl:flex-1 xl:w-auto p-4 bg-gradient-to-b from-[rgba(0,0,0,)] to-black flex-col justify-end items-center gap-4 inline-flex">
       <img
         className="absolute top-0 left-0 w-full h-full object-cover rounded-lg"
         src={imageUrl}
         alt={title + " promation image"}
       />
 
-      <div className="relative self-stretch h-8 line-clamp-1 text-center text-white text-2xl font-bold font-['Poppins'] leading-loose">
+      <div className="relative self-stretch h-8 line-clamp-1 text-center text-white text-2xl font-bold leading-loose">
         {title}
       </div>
-      <div className="relative self-stretch text-center text-white text-base font-normal font-['Poppins'] leading-tight">
+      <div className="relative self-stretch text-center text-white text-base font-normal leading-tight">
         {description}
       </div>
       <Link
-        to={`/clothing/&${permalink}`}
+        to={`/clothing/all-men&${permalink}`}
         className="relative h-[54px] px-2.5 py-[7px] bg-[#222222] justify-center items-center gap-2 inline-flex"
       >
-        <div className="text-white text-base font-normal font-['Poppins'] leading-tight">
+        <div className="text-white text-base font-normal leading-tight">
           View more
         </div>
       </Link>
@@ -37,14 +37,7 @@ const CollectionSection = () => {
   const [page, setPage] = useState(0);
   const { name, description, items, permalink } = collectionList?.[page] ?? {
     name: "Collections",
-    description: `            Step into a world of winter elegance and style with our latest
-            Winter Collection. As temperatures drop, our curated selection of
-            clothing is designed to keep you fashionably warm. From luxurious
-            knitwear to trend-setting outerwear, each piece in our collection is
-            a celebration of seasonal sophistication. Explore the blend of
-            comfort and fashion, as we present you with the must-have ensembles
-            to make a statement in the chilly months ahead. Welcome to a winter
-            wardrobe that seamlessly combines coziness with chic aesthetics.`,
+    description: "",
     items: [],
     permalink: "",
   };
@@ -139,16 +132,16 @@ const CollectionSection = () => {
     <section className="py-16 px-[1.125rem] lg:px-32">
       <div className="w-full flex flex-col xl:flex-row justify-center items-center gap-16">
         {/* Left Column (Title & Description) */}
-        <div className="h-[458px] flex-col justify-start items-start gap-6 inline-flex">
+        <div className="h-[458px] flex-col justify-start items-start gap-6 inline-flex max-w-[575px] xl:flex-1">
           <div className="flex-col justify-start items-start flex">
-            <div className="text-[#222222] text-8xl font-bold font-['Poppins'] leading-[116px]">
+            <div className="text-[#222222] text-8xl font-bold leading-[116px]">
               2024
             </div>
-            <div className="text-[#222222] text-5xl font-bold font-['Poppins'] leading-[72px]">
+            <div className="text-[#222222] text-5xl font-bold leading-[72px]">
               {name}
             </div>
           </div>
-          <div className="w-[363px] text-[#222222] text-base font-normal font-['Poppins'] leading-tight">
+          <div className="text-[#222222] text-base font-normal leading-tight">
             {description}
           </div>
         </div>
