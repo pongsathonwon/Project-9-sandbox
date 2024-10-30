f682cc8c46288e78dfcca56c76f61411541c3ebc
pongsathonwon <men1234-2008@hotmail.com>
2024-10-31 03:50:30 +0700

    update sidebar

--- src/components/header/Sidebar.jsx
+++ src/components/header/Sidebar.jsx
@@ -1,28 +1,15 @@
 import React from "react";
-import {
-  Link,
-  NavLink,
-  useLocation,
-  useNavigate,
-  useParams,
-} from "react-router-dom";
-import {
-  genClothingList,
-  navlist,
-  secondaryNavlist,
-  tertiaryNavlist,
-} from "./navlist";
+import { NavLink, useLocation, useNavigate, useParams } from "react-router-dom";
+import { genClothingList, navlist, secondaryNavlist } from "./navlist";
 import Arrow from "../Icon/Arrow";
 import { useCollectionContext } from "../../context/CollectionContextProvider";
+import Icon from "../Icon/Icon";
+import Cross from "../Icon/Cross";
 
 const ExpandableButton = ({ labelText, children }) => {
   const [open, setOpen] = React.useState(false);
   return (
-    <div
-      aria-haspopup="listbox"
-      className="relative w-full h-full"
-      onBlur={() => setOpen((p) => !p)}
-    >
+    <li aria-haspopup="listbox" className="relative w-full h-full">
       <button
         onClick={() => setOpen((p) => !p)}
         className="capitalize text-lg font-semibold w-full h-full text-start flex justify-between items-center"
@@ -30,27 +17,29 @@ const ExpandableButton = ({ labelText, children }) => {
         {labelText}
         <Arrow direction={open ? "down" : "up"} />
       </button>
-      <ul className={`${open ? "" : "scale-y-0 duration-300 origin-top"}`}>
+      <ul
+        className={`${
+          open ? "" : "scale-y-0 duration-300 origin-top"
+        } flex flex-col gap-2 pt-2`}
+      >
         {children}
       </ul>
-    </div>
+    </li>
   );
 };
 
 function Sidebar({ isShow, onClick }) {
   const { possibleCollectionList } = useCollectionContext();
   const [secondary, setSecondary] = React.useState(null);
-  const [secPath, setSecPath] = React.useState(null);
-  const [tertiary, setTertiary] = React.useState(null);
-  const navigate = useNavigate();
   const { type } = useParams();
-  React.useEffect(() => {
-    if (!type) return;
-    const cat = type.split("&")[0];
-    const cur = navlist.find(({ path }) => path === cat);
-    if (cur) setSecondary(cur.label);
-    setSecPath(cat);
-  }, [type]);
+  const [baseCat, baseCol] = type ? type.split("&") : [null, null];
+  const navigate = useNavigate();
+  const getCategoriesUrl = (categories = "") =>
+    `clothing/${categories === "" ? "" : categories}${
+      baseCol ? "&" + baseCol : ""
+    }`;
+  const getCollectionUrl = (collection = "") =>
+    `clothing/${baseCat ?? ""}${collection === "" ? "" : "&" + collection}`;
   return (
     <div
       className={`fixed top-0 left-0 right-0 h-screen bg-black bg-opacity-50 flex transition-all duration-300 z-20 ${
@@ -63,22 +52,29 @@ function Sidebar({ isShow, onClick }) {
           <div
             aria-haspopup="listbox"
             className={`absolute duration-300 h-screen ${
-              secondary || tertiary ? "-translate-x-full" : ""
+              secondary ? "-translate-x-full" : ""
             }`}
           >
-            <button
-              className="font-bold capitalize flex gap-8 items-center w-full px-4 h-12 text-lg"
-              onClick={onClick}
-            >
-              <span className="text-lg ml-4">home</span>
-            </button>
+            <div className="font-bold capitalize flex gap-8 items-center w-full px-8 h-12 text-lg">
+              <NavLink to="/" className="text-lg flex-1">
+                home
+              </NavLink>
+              <button
+                onClick={onClick}
+                className=" rounded-full overflow-hidden hover:bg-secondary-100"
+              >
+                <Icon>
+                  <Cross />
+                </Icon>
+              </button>
+            </div>
             <ul className="flex flex-col gap-2 text-lg font-semibold px-8 capitalize">
               {navlist.map(({ label, path }) => (
                 <li className="w-64 flex items-center h-12" key={path}>
                   <button
                     onClick={() => {
+                      navigate(genClothingList(path));
                       setSecondary(label);
-                      setSecPath(path);
                     }}
                     className="w-full h-full text-start capitalize"
                   >
@@ -92,74 +88,57 @@ function Sidebar({ isShow, onClick }) {
           <div
             aria-haspopup="listbox"
             className={`aboslute duration-300 h-screen ${
-              secondary && !tertiary ? "" : "-translate-x-full"
+              secondary ? "" : "-translate-x-full"
             }`}
           >
-            <button
-              className="font-bold capitalize flex gap-8 items-center w-full px-4 h-12 border-b border-secondary-300 text-2xl"
-              onClick={() => setSecondary(null)}
-              aria-haspopup=""
-            >
-              <Arrow direction="left" />
-              {secondary}
-            </button>
+            <div className="font-bold flex items-center w-full px-4 h-12 border-b border-secondary-300 text-2xl">
+              <button
+                className="flex gap-8 items-center flex-1 capitalize"
+                onClick={() => setSecondary(null)}
+                aria-haspopup=""
+              >
+                <Arrow direction="left" />
+                {secondary}
+              </button>
+              <button
+                onClick={onClick}
+                className=" rounded-full overflow-hidden hover:bg-secondary-100 mr-4"
+              >
+                <Icon>
+                  <Cross />
+                </Icon>
+              </button>
+            </div>
             <ul className="flex flex-col gap-2 text-lg font-semibold px-8 capitalize">
+              <li className="flex w-64 items-center justify-between text-lg font-semibold h-12">
+                Categories
+              </li>
               {secondaryNavlist(secondary)?.map(({ label, path }) => (
                 <li key={path} className="listitem">
                   <NavLink
-                    className="flex w-64 h-12 items-center justify-between font-normal"
-                    to={genClothingList(path)}
+                    className={`flex w-64 h-12 items-center justify-between font-normal ${
+                      type?.split("&")[0] === path ? "active" : ""
+                    }`}
+                    to={getCategoriesUrl(path)}
                   >
                     {label}
                   </NavLink>
                 </li>
               ))}
-              <li className="px-2.5 h-12">
-                <ExpandableButton labelText="Collections">
-                  {possibleCollectionList.map(({ label, path }) => (
-                    <li
-                      key={path}
-                      className="h-12 full flex items-center pl-4 hover:bg-none text-base font-normal"
+              <ExpandableButton labelText="Collections">
+                {possibleCollectionList.map(({ label, path }) => (
+                  <li key={path} className="listitem">
+                    <NavLink
+                      to={getCollectionUrl(path)}
+                      className={`flex w-full h-12 items-center justify-between font-normal ${
+                        type?.includes(path) ? "active" : ""
+                      }`}
                     >
-                      <button
-                        className="h-full w-full text-start"
-                        onClick={() => {
-                          setTertiary({ label, path });
-                          navigate(genClothingList(`${secPath}&${path}`));
-                        }}
-                      >
-                        {label}
-                      </button>
-                    </li>
-                  ))}
-                </ExpandableButton>
-              </li>
-            </ul>
-          </div>
-          {/* tertiary sidebar */}
-          <div
-            className={`aboslute duration-300 h-screen -translate-y-full ${
-              tertiary ? "" : "-translate-x-full"
-            }`}
-          >
-            <button
-              className="font-bold capitalize flex gap-8 items-center w-full px-4 h-12 border-b border-secondary-300 text-2xl"
-              onClick={() => setTertiary(null)}
-            >
-              <Arrow direction="left" />
-              {tertiary?.label}
-            </button>
-            <ul className="flex flex-col gap-2 text-lg font-semibold px-8 capitalize">
-              {tertiaryNavlist(tertiary?.path)?.map(({ label, path }) => (
-                <li key={path} className="listitem">
-                  <NavLink
-                    className="flex w-64 h-12 items-center justify-between font-normal"
-                    to={genClothingList(path)}
-                  >
-                    {label}
-                  </NavLink>
-                </li>
-              ))}
+                      {label}
+                    </NavLink>
+                  </li>
+                ))}
+              </ExpandableButton>
             </ul>
           </div>
         </div>
